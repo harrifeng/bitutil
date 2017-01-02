2878f206909359ec3e5579af2f238f6be208fcd8
Harri Feng <harrifeng@gmail.com>
2017-01-02 11:36:46 +0800

    test the result

--- bitutil_test.go
+++ bitutil_test.go
@@ -25,3 +25,24 @@ func TestTwoComplementInt8ToRaw(t *testing.T) {
 		})
 	}
 }
+
+func TestHexStringToBinaryString(t *testing.T) {
+	type args struct {
+		x string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Test All hex string", args{"ABCDEF"}, "1010|1011|1100|1101|1110|1111"},
+		{"Test All hex number", args{"0123456789"}, "0000|0001|0010|0011|0100|0101|0110|0111|1000|1001"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HexStringToBinaryString(tt.args.x); got != tt.want {
+				t.Errorf("HexStringToBinaryString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
